d93926b66e8cbddac5d92a577877ca82ada596c9
npham014 <npham014@ucr.edu>
2018-01-26 12:42:22 -0800

    switched direction of light ray vector and normald

--- src/phong_shader.cpp
+++ src/phong_shader.cpp
@@ -20,7 +20,8 @@ Shade_Surface(const Ray& ray,const vec3& intersection_point,
 
    
     for(unsigned int i = 0; i < world.lights.size(); ++i) {//Loop through lights
-	lightRay.direction =intersection_point - world.lights.at(i)->position; //12:40 Switched these 2 variables. 
+	lightRay.direction = intersection_point - world.lights.at(i)->position;
+	lightRay.direction = lightRay.direction.normalized();
 	lightRay.endpoint = world.lights.at(i)->position;
 	if(world.enable_shadows) {
 		Hit passToShadow;
