a20c0f75f35bbb545b545a3e976c47e50ea2589d
Nathan Pham <npham014@wch110-01.cs.ucr.edu>
2018-01-25 16:59:59 -0800

    bugfixing phong shader

--- src/phong_shader.cpp
+++ src/phong_shader.cpp
@@ -9,13 +9,31 @@ Shade_Surface(const Ray& ray,const vec3& intersection_point,
     const vec3& same_side_normal,int recursion_depth,bool is_exiting) const 
 {
     vec3 color;
-    color += color_ambient + (world.ambient_color * world.ambient_intensity);
+    color = color_ambient * (world.ambient_color * world.ambient_intensity);
     
     vec3 tempDiff;
-    if(is_exiting) { //If the ray point is exiting that means the view is inside the object? this means we have to turn it around
+
+    // Move this to Cast_Ray
+   /* if(is_exiting) { //If the ray point is exiting that means the view is inside the object? this means we have to turn it around
       same_side_normal *= -1; //Flip the vector around if it's facing the wrong way? 
       //Note:Not really sure why I'm even checking this if my normals are passed in properly. 
+    }*/
+    Ray lightRay;
+    vec3 lightColor;
+    for(unsigned int i = 0; i < world.lights.size(); ++i) {//Loop through lights
+	//Calculate Diffuse and Add to color
+	lightRay.direction = world.lights.at(i)->position - intersection_point;
+	lightColor = world.lights.at(i)->Emitted_Light(lightRay);
+	color += lightColor * color_diffuse * max(dot(same_side_normal,lightRay.direction.normalized()),0);
+
+        //Calculate Specular and Add to color
+       vec3 reflectDirection = (2 * dot(lightRay.direction.normalized(), same_side_normal) * (same_side_normal - lightRay.direction.normalized()));
+       double specInt = max(dot(reflectDirection,ray.direction.normalized()), 0);
+       specInt = pow(specInt, specular_power);
+       color += (lightColor * color_specular * specInt);
+
     }
-    for(unsigned int i = 0; i < world.
+
+    
     return color;
 }
--- src/render_world.cpp
+++ src/render_world.cpp
@@ -72,7 +72,10 @@ vec3 Render_World::Cast_Ray(const Ray& ray,int recursion_depth)
 
     if(foundObject) {
 	vec3 intersection = ray.Point(temp);
-        vec3 normal = foundObject.Normal();
+        vec3 normal = foundObject.Normal(intersection);
+	if(temp.ray_exiting) {
+		normal *= -1;
+	}
 	color = foundObject->material_shader->Shade_Surface(ray,intersection,normal,recursion_depth, temp.ray_exiting);
     }
     else {
