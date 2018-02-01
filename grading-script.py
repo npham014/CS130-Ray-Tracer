687c456ba4f7fa2a2efde04c5603e09b94dd59e9
Nathan Pham <npham014@wch110-02.cs.ucr.edu>
2018-02-01 15:15:37 -0800

    updated grading script

--- grading-script.py
+++ grading-script.py
@@ -107,14 +107,23 @@ for line in gs.readlines():
         if not run_command_with_timeout(grade_cmd, timeout):
             hashed_tests[file]=("TIMEOUT",None)
         else:
-            results_file=open(dir+'/'+token+'.txt')
-            d=diff_parse.match(results_file.readline())
-            results_file.close()
-            if d: d=float(d.groups()[0])
+            d=False
+            try:
+              results_file=open(dir+'/'+token+'.txt')
+              d=diff_parse.match(results_file.readline())
+              results_file.close()
+              os.remove(dir+'/'+token+'.txt')
+              if d: d=float(d.groups()[0])
+            except IOError:
+              # print 'Test failed'
+              d="CRASH"
             hashed_tests[file]=d
 
     d=hashed_tests[file]
-    if d=="TIMEOUT":
+    if d=="CRASH":
+        print("FAIL: (%s) Program crashed."%file)
+        points=0
+    elif d=="TIMEOUT":
         print("FAIL: (%s) Test timed out."%file)
         points=0
     elif d==None:
@@ -134,4 +143,3 @@ for line in gs.readlines():
         print("no points")
 
 print("FINAL SCORE: %g"%total_score)
-
